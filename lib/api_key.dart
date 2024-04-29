6d668077b90ac7b1de0bac0ab85cbca894140f3a
Mickael DP <m.dallepasqualine@gmail.com>
2024-04-29 16:12:58 +0200

    mise en place homepage

---
+++ lib/api_key.dart
@@ -0,0 +1,1 @@
+const apiKey = "1aae0290d5c4cb377e9cd83395c1914a";
---
+++ lib/home_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mars Rover Photos'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Card(
+              color: Color.fromARGB(255, 183, 183, 183),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(''),
+                    Text(''),
+                    Text(''),
+                    Text(''),
+                    Text(''),
+                    Text(''),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
